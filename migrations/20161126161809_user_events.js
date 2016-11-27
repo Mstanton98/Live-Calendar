95d316a966232fdc1c46554d1ce9d58b04c6fb1c
Mstanton98 <mstanton98@gmail.com>
2016-11-26 16:42:17 -0800

    added migrations and JWT secret

---
+++ migrations/20161126161809_user_events.js
@@ -0,0 +1,22 @@
+'user strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('user_events', (table) => {
+    table.increments();
+    table.integer()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .index();
+    table.string('artist_name').notNullable().defaultTo('');
+    table.string('venue_name').notNullable().defaultTo('');
+    table.string('event_date').notNullable().defaultTo('');
+    table.integer('event_id').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('user_events');
+};
