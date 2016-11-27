bd1038098fba4a4be4f63962520944230ffdbec1
Mstanton98 <mstanton98@gmail.com>
2016-11-26 20:37:03 -0800

    added user route

---
+++ routes/users.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// eslint-disable-next-line new-cap
+
+const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
+const express = require('express');
+const knex = require('../knex');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+const ev = require('express-validation');
+const validations = require('../validations/users');
+
+const router = express.Router();
+
+router.post('/users', ev(validations.post), (req, res, next) => {
+  const { firstName, lastName, username, email, password } = req.body;
+
+  knex('users')
+    .where('email', email)
+    .then((row) => {
+      if (row.length) {
+        return next(boom.create(400, 'Invalid username or password.'));
+      }
+    });
+
+  bcrypt.hash(password, 12)
+    .then((hashedPassword) => {
+      const insertUser = { firstName, lastName, username, email, hashedPassword };
+
+      return knex('users').insert(decamelizeKeys(insertUser), '*');
+    })
+    .then((rows) => {
+      const user = camelizeKeys(rows[0]);
+
+      delete user.hashedPassword;
+
+      res.send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
